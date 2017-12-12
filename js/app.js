5163cfff724b5183eb20ce952d8df1958600a19e
cpdrenato <cpdenato@gmail.com>
2017-12-12 00:42:59 -0200

    scholl

---
+++ js/app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by renato on 12/12/17.
+ */
+var hello = new Vue({
+    el:'#hello',
+    data:{
+        msg:"Hello Vue!",
+        peoples:[
+            {name:"Leidy"},
+            {name:"Alexandre"},
+            {name:"Renato"},
+            {name:"Ana"},
+            {name:"Laurinha"}
+        ]
+    }
+}
+
+)
